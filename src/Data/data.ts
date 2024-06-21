abcbd0639712f89273ffed1edc34352b92be6d96
imtaij <imtiajsk635@gmail.com>
2024-06-21 18:00:38 +0530

    Add files via upload

---
+++ src/Data/data.ts
@@ -0,0 +1,35 @@
+export let menu = [
+  {
+    id: 1,
+    title: "Free Spins",
+    path: "/",
+  },
+];
+export let fandq = [
+  {
+    title: "Do reward links expire or not?",
+    text: "Yes, Coin Master free spins and coins reward links will expire in the next three days. Then you can't claim the reward mentioned. So, collect the spins and coins immediately.",
+  },
+  {
+    title: "How to get Coin Master Spins daily?",
+    text: "You can easily get or collect those free spins and coins daily. For this, visit this page or open the official Facebook page. Then tap on the link and the prizes will credited to the game inventory.",
+  },
+  ,
+  {
+    title: "What are the benefits of free spins and coins?",
+    text: "Spins and Coins are very crucial elements here and it help users to use Slot machines as well as build the village. If you don't have the spins then you need to spend real money to purchase them to continue. So, a better way is the free spin and coin rewards.",
+  },
+  ,
+  {
+    title: "How to earn maximum coins?",
+    text: "To get maximum coins, you need to have the best plan and strategy. For this, attack and raid opponent villages. Also, utilize Foxy and Tiger pets for more advantage during attacking and collect maximum coins.",
+  },
+];
+export let carousel = [
+  "https://play-lh.googleusercontent.com/oK2aQdfnB27hxbPlxFpVFA9CvRaaWrvHVqQOUDDzDEuP81X8Ny3dyl-FL90JPNk2P9Q=w526-h296-rw",
+  "https://play-lh.googleusercontent.com/NGP0V2gPacNQfv9kfrjshIlLbsZu-wNInKydFx7mydFGJJFQjjLwPHbhV3LmleTwmLbB=w526-h296-rw",
+  "https://play-lh.googleusercontent.com/o1ymVTjbIt3N6B720RMy1Rluqz3Q5QzICPmOmSzkl97spBO1rTH4ECBYm4o5p9WvZak=w526-h296-rw",
+  "https://play-lh.googleusercontent.com/ymvDJn6dzFHtYVCNTNXV7nKhQY7dqqaY_oNxo68QoG9h8Ck7sR6sA5KRxY7vRqA7-pg=w526-h296-rw",
+  "https://play-lh.googleusercontent.com/MmtIpkEyKPdI839hGmzR2eL4N0aUhZuRQlppXKXvByROSfYqVzJTSUxHE5lWu2mGZgI=w526-h296-rw",
+  "https://play-lh.googleusercontent.com/ZTVgB9BAZVub14Ba7JDLm_nek2adRm6Guf7ZdN6zrYIAld2C2o5_aaKmBYUTaO8XZj0=w526-h296-rw",
+];
